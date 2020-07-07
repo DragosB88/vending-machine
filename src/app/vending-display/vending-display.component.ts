87f7067d3a7c936738228a062c25bd0e535ed4f6
Dragos <s.dragosbodea@gmail.com>
2020-07-07 14:18:37 +0300

    added styling with dynamic product classes

--- src/app/vending-display/vending-display.component.ts
+++ src/app/vending-display/vending-display.component.ts
@@ -8,13 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendingDisplayComponent implements OnInit {
   products;
+  vendingSlots = [];
   constructor(ProductsService: ProductsService) {
     this.products = ProductsService.getProducts();
+
+    // insert items into vending machine
+    for (const item of this.products) {
+      if (item.qty > 10) {
+        const diffQtyItem = Object.assign({}, item, { qty: 10 });
+        this.vendingSlots.push(diffQtyItem);
+
+        item.qty = item.qty - 10;
+        this.vendingSlots.push(item);
+      } else {
+        this.vendingSlots.push(item);
+      }
+    }
+
+    // make sure that the number of items displayed is not bigger than the available vending machine slots
+    if (this.vendingSlots.length >= 20) {
+      console.log('Vending machine display limit exceeded');
+      this.vendingSlots.splice(20);
+    } else if (this.vendingSlots.length === 20) {
+      console.log('Vending machine display has reached its limit');
+    } else {
+      console.log('Vending machine display still has available item slots');
+    }
+    console.log('final array ', this.vendingSlots);
   }
 
   ngOnInit(): void {}
-
-  // let vendingMerchandise = {
-
-  // }
 }
